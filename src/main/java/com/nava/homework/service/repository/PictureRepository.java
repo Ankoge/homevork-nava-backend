29bd5c01c082f3f0fa94c0d85028b64529a16cae
Kovacs Gergő András <kovacs.gergo.andras@gmail.com>
2022-12-28 14:50:57 +0100

    Create JPA repositories.

---
+++ src/main/java/com/nava/homework/service/repository/PictureRepository.java
@@ -0,0 +1,9 @@
+package com.nava.homework.service.repository;
+
+import com.nava.homework.model.Picture;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PictureRepository extends JpaRepository<Picture, Long> {
+}
